240cbd28dc546eeff260b84ce7c8c05a2aeb482f
Leonardo <lrossi@localhost.localdomain>
2019-06-14 21:24:39 -0300

    Limpando código/comentários

--- app/src/main/java/br/edu/ifsp/financasdroid/controller/fragment/ChartFragment.java
+++ app/src/main/java/br/edu/ifsp/financasdroid/controller/fragment/ChartFragment.java
@@ -1,30 +1,27 @@
 package br.edu.ifsp.financasdroid.controller.fragment;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import br.edu.ifsp.financasdroid.R;
 import br.edu.ifsp.financasdroid.model.TransactionType;
-import br.edu.ifsp.financasdroid.model.entity.Category;
 import br.edu.ifsp.financasdroid.model.entity.Transaction;
 import br.edu.ifsp.financasdroid.model.service.TransactionService;
 
@@ -34,7 +31,7 @@ public class ChartFragment extends Fragment {
 
     private TransactionService transactionService;
 
-    private int[] COLORS = new int[] {
+    private int[] COLORS = new int[]{
             R.color.green900,
             R.color.green700,
             R.color.green500,
@@ -65,16 +62,6 @@ public class ChartFragment extends Fragment {
         transactionService = new TransactionService(getContext());
         Map<String, List<Transaction>> transactions = transactionService
                 .findByTypeSortedByCategory(TransactionType.DEBIT);
-
-//        ArrayList<PieEntry> yValues = new ArrayList<>();
-//
-//        yValues.add(new PieEntry(34f, "Brasil"));
-//        yValues.add(new PieEntry(23f, "EUA"));
-//        yValues.add(new PieEntry(14f, "Japão"));
-//        yValues.add(new PieEntry(35, "Alemanha"));
-//        yValues.add(new PieEntry(40, "China"));
-//        yValues.add(new PieEntry(23, "Rússia"));
-
 
         List<PieEntry> pieData = getPieData(transactions);
         PieDataSet dataSet = new PieDataSet(pieData, "Débitos");
@@ -107,7 +94,7 @@ public class ChartFragment extends Fragment {
 
     private float calcTransactionsValue(final List<Transaction> transactions) {
         float total = 0;
-        for (Transaction t: transactions) {
+        for (Transaction t : transactions) {
             if (t != null && t.getValue() != null) {
                 total += t.getValue();
             }
